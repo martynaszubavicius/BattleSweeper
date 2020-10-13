81a5e02f790469dba243f20f4494e0bcc3a93123
Martynas Zubavičius <martynasz159@gmail.com>
2020-10-14 00:58:00 +0300

    Redone client side drawing, the window is now responsive to resizing. Hurray.

---
+++ BattleSweeperClient/FormPainter.cs
@@ -0,0 +1,44 @@
+﻿using BattleSweeperServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BattleSweeperClient
+{
+    class FormPainter
+    {
+        private Panel panel;
+        private GameSettings gameSettings;
+
+        // Player bounds
+        private RectangleF playerBoard;
+        private RectangleF playerMines; // 39x23
+        private RectangleF playerAmmo;
+
+        // Enemy bounds
+        private RectangleF enemyBoard;
+        private RectangleF enemyMines;
+        private RectangleF enemyAmmo;
+
+
+        public FormPainter(Panel panel, GameSettings gameSettings)
+        {
+            this.panel = panel;
+            this.gameSettings = gameSettings;
+        }
+
+        
+
+        public void DrawGame(Game game)
+        {
+
+        }
+
+
+
+    }
+}
