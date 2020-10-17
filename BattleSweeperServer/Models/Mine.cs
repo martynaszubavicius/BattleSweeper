48e59ad57f9e436f31548374682d37487a041850
Lukas <pupelislukas053@gmail.com>
2020-10-17 15:00:46 +0300

    Different mine explosions, made Mine an abstract class

--- BattleSweeperServer/Models/Mine.cs
+++ BattleSweeperServer/Models/Mine.cs
@@ -1,10 +1,17 @@
-﻿namespace BattleSweeperServer.Models
+﻿using System.Data.Entity.Core.Mapping;
+
+namespace BattleSweeperServer.Models
 {
-    public class Mine
+    public abstract class Mine
     {
         public string ImageName { get; set; }
 
         public Mine()
+        {
+
+        }
+
+        public virtual void OnReveal(Board board, int x, int y)
         {
 
         }
--- BattleSweeperServer/Models/WideMine.cs
+++ BattleSweeperServer/Models/WideMine.cs
@@ -11,5 +11,15 @@ namespace BattleSweeperServer.Models
         {
             ImageName = "red_bomb";
         }
+
+        public override void OnReveal(Board board, int x, int y)
+        {
+            int radius = 2;
+
+            for (int i = x - radius + 1; i < x + radius; i++)
+                for (int j = y - radius + 1; j < y + radius; j++)
+                    if (board.WithinBounds(i, j))
+                        board.RevealTile(i, j);
+        }
     }
 }
