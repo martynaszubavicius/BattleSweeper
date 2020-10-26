e1b95a978e58350fc28e5227a91df73b5d88aeee
Devilling <edvinas.bukota@ktu.edu>
2020-10-26 20:46:01 +0200

    Added Observer empty class.

---
+++ BattleSweeperServer/DesignPatternClasses/Observer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BattleSweeperServer.DesignPatternClasses
+{
+    public class Observer
+    {
+        List<Command> History = new List<Command>();
+
+        public void Add(Command command)
+        {
+            History.Add(command);
+        }
+
+
+
+        //private List<Command> GetPlayerViewCommands(string playerIdentifier, int historyStartIndex)
+        //{
+        //    List<Command> commands = this.History
+        //        .GetRange(historyStartIndex, this.History.Count - historyStartIndex)
+        //        .Where(item => !(item.PlayerId != playerIdentifier && item.Info.CommandType == "mine"))
+        //        .ToList();
+
+        //    // clean up the player id's from enemy commands - in order to not ruin data, 
+        //    // we need to create new command objects with null values in place of id
+        //    for (int i = 0; i < commands.Count; i++)
+        //    {
+        //        if (commands[i].Info.CommandType == "shot" && commands[i].PlayerId != playerIdentifier)
+        //            commands[i] = new ShotCommand(commands[i].Info, default) { Points = commands[i].Points };
+        //    }
+
+        //    return commands;
+        //}
+    }
+}
