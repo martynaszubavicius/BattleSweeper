225febcdaebd7ba7399c393345cd3ac9ddd85134
Lukas <pupelislukas053@gmail.com>
2020-12-04 22:25:04 +0200

    sudetingesnis testas

--- BattleSweeperServer/DesignPatternClasses/LineShotBehaviourTests.cs
+++ BattleSweeperServer/DesignPatternClasses/LineShotBehaviourTests.cs
@@ -1,21 +1,53 @@
 using BattleSweeperServer.Models;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using System;
 
 namespace BattleSweeperServer.DesignPatternClasses
 {
-    [TestClass]
+    [TestFixture]
     public class LineShotBehaviourTests
     {
-        [TestMethod]
+        [Test]
         public void Shoot_StateUnderTest_ExpectedBehavior()
         {
             // Arrange
             var lineShotBehaviour = new LineShotBehaviour(5);
-            
+            Board board = new Board(10);
+            int x = 0;
+            int y = 0;
+
+            //// Act
+            var result = lineShotBehaviour.Shoot(
+                board,
+                x,
+                y);
+
+            ChangePointComposite comp = new ChangePointComposite(0, 0);
+            comp.Add(new ChangePointLeaf(0, 0));
+            comp.Add(new ChangePointLeaf(1, 0));
+            comp.Add(new ChangePointLeaf(2, 0));
 
             // Assert
-            Assert.AreEqual(true, true);
+            Assert.AreEqual(result, comp);
+        }
+
+        [Test]
+        public void Shoot_StateUnderTest_aaaExpectedBehavior()
+        {
+            // Arrange
+            var lineShotBehaviour = new LineShotBehaviour(5);
+
+            // Assert
+            Assert.AreEqual(1, 1);
+        }
+
+        [Test]
+        public void aaaFail()
+        {
+            // Arrange
+            var lineShotBehaviour = new LineShotBehaviour(5);
+            // Assert
+            Assert.Fail();
         }
     }
 }
